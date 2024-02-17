5b6ffe2e5fb018b15a6ea738c29ab4a8e87381d0
shogirdeveloper <shogirdeveloper.info@gmail.com>
2024-02-17 13:37:56 +0500

    fixed first basic javascript

---
+++ index.js
@@ -0,0 +1,143 @@
+var number = 5; // in-line comment
+
+/* 
+var number = 90;
+var age = 15;
+var name = "Ali"
+multi-line comments */
+
+// Data types
+// number
+// string
+// undefined
+// null
+// boolean
+// symbol
+// Object
+// BegInt
+
+// -------------
+var myName = "Ali";
+
+myName = 8;
+
+let ourName = "shogirdev";
+
+const myChannelName = "Shogird Developer";
+
+// -------------
+
+var a;
+var b = 2;
+// console.log(a);
+a = 7;
+b = a;
+
+//-------------
+
+// Ushbu uchta variable ichini to'g'ri to'ldiring
+
+var a;
+var b;
+var c;
+
+// Ushbu satrlarni o'zgartirmang
+
+a = a + 1;
+b = b + 5;
+c = c + " String!"
+
+
+// increment
+var myVar = 87;
+myVar = myVar + 1;
+// myVar++
+
+// decrement
+var myVar = 11;
+myVar = myVar - 1;
+// myVar--
+
+
+// Remainder numbers - qoldiq qoladigan sonlar 
+
+var remainder;
+remainder = 10 % 2;
+// console.log(remainder);
+
+var a = 12;
+var b = 2;
+var c = 13;
+
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+// a += 12
+// b += 9
+// c += 7
+
+
+var a = 12;
+var b = 2;
+var c = 13;
+
+a = a - 12;
+b = 9 - b;
+c = c - 7;
+// a -= 12
+// b -= 9
+// c -= 7
+
+var a = 12;
+var b = 2;
+var c = 13;
+
+a = a / 12;
+b = 9 / b;
+c = c / 7;
+// a /= 12
+// b /= 9
+// c /= 7
+
+var a = 12;
+var b = 2;
+var c = 13;
+
+a = a * 12;
+b = 9 * b;
+c = c * 7;
+// a *= 12
+// b *= 9
+// c *= 7
+
+// Stringlar
+
+// var myStr = "Bu mening birinchi so'zim. ";
+// myStr += "Bu mening ikkinchi so'zim.";
+// console.log(myStr);
+
+// Example
+// var ourNames = "shogirdeveloper";
+// var ourStr = "Hello, our name is " + ourNames + " and I am well!";
+
+// var myName = "Ali";
+// var myStr = "My name is " + myName + " and I'm well!";
+// console.log(myStr);
+
+
+// Stringlarning length ni aniqlash
+
+// var firstNameLength = 0;
+// var firstName = "Odil";
+
+// firstNameLength = firstName.length;
+// console.log(firstNameLength);
+
+var lastNameLength = 0;
+var lastName = "Saydullayev";
+
+lastNameLength = lastName.length;
+console.log(lastNameLength);
+
+
+
